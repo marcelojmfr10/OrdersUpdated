2a122c3040a60b28af381db872be6cf172ecaa2a
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-01-04 21:06:14 -0600

    Add generic repository, unit of work and controller

--- Orders/Orders.Backend/Controllers/CountriesController.cs
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -1,61 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Orders.Backend.Data;
+using Orders.Backend.UnitsOfWork.Interfaces;
 using Orders.Shared.Entities;
 
 namespace Orders.Backend.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class CountriesController : ControllerBase
+public class CountriesController : GenericController<Country>
 {
-    private readonly DataContext _context;
-
-    public CountriesController(DataContext context)
+    public CountriesController(IGenericUnitOfWork<Country> unitOfWork) : base(unitOfWork)
     {
-        _context = context;
-    }
-
-    [HttpGet]
-    public async Task<IActionResult> GetAsync()
-    {
-        return Ok(await _context.Countries.ToListAsync());
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> PostAsync(Country country)
-    {
-        _context.Add(country);
-        await _context.SaveChangesAsync();
-        return Ok(country);
-    }
-
-    [HttpPut("{id:int}")]
-    public async Task<IActionResult> PutAsync(int id, Country country)
-    {
-        var countryDb = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
-        if(countryDb is null)
-        {
-            return NotFound();
-        }
-
-        countryDb.Name = country.Name;
-        _context.Update(countryDb);
-        await _context.SaveChangesAsync();
-        return Ok(countryDb);
-    }
-
-    [HttpDelete("{id:int}")]
-    public async Task<IActionResult> DeleteAsync(int id, Country country)
-    {
-        var countryDb = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
-        if (countryDb is null)
-        {
-            return NotFound();
-        }
-
-        _context.Remove(countryDb);
-        await _context.SaveChangesAsync();
-        return NoContent();
     }
 }
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Orders.Backend.Data;
+using Orders.Backend.Repositories.Implementations;
+using Orders.Backend.Repositories.Interfaces;
+using Orders.Backend.UnitsOfWork.Implementantios;
+using Orders.Backend.UnitsOfWork.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +15,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 
 var app = builder.Build();
 
